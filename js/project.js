ae3a871ba9de0212504fed21725f37a3ebf9543d
krystalloh <krystal.j.loh@gmail.com>
2015-06-26 07:33:38 -0400

    all the things

---
+++ js/project.js
@@ -0,0 +1,9 @@
+$(function(){
+
+$('#logo').fadeIn({queue: false, duration: 2000});
+$('#logo').animate({ left: "30px" }, 1000);
+
+$('#navbar').fadeIn({queue: false, duration: 2000});
+//$('#navbar').animate({ right: "10px" }, 1000);
+
+});
