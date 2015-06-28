e3a8a141124793ed34e1e0fd5e23a3991d8b3557
krystalloh <krystal.j.loh@gmail.com>
2015-06-28 15:53:03 -0400

    modify layout

--- js/project.js
+++ js/project.js
@@ -1,9 +1,20 @@
 $(function(){
 
-$('#logo').fadeIn({queue: false, duration: 2000});
-$('#logo').animate({ left: "30px" }, 1000);
+setTimeout(function(){
+
+$('#logo').fadeIn({
+	queue: false, 
+	duration: 2000
+});
+
+$('#logo').animate({ 
+	left: "30px" }, 1000
+	);
 
 $('#navbar').fadeIn({queue: false, duration: 2000});
-//$('#navbar').animate({ right: "10px" }, 1000);
+$('#navbar').animate({ right: "10px" }, 1000);
 
-});
+3000 }); // setTIMEOUT END
+
+
+}); // END DOCUMENT.READY
