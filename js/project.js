05648aa48110f1047680eab9a3af0e8cb4e19805
krystalloh <krystal.j.loh@gmail.com>
2015-06-30 20:10:13 -0400

    add fancybox and flickr

--- js/project.js
+++ js/project.js
@@ -16,5 +16,34 @@ $('#navbar').animate({ right: "10px" }, 1000);
 
 3000 }); // setTIMEOUT END
 
+//FLICKR PHOTOSET
+
+var apiKey = '0d5486f9e206f51751c1994df4f4c7cd',
+	graphics_id = '72157652951691994',
+	flickr = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key='+apiKey+'&photoset_id='+graphics_id+'&format=json&jsoncallback=?';
+
+	$.getJSON(flickr, function(response){
+        // console.log(response);
+
+        $.each(response.photoset.photo, function(i, photoInfo){
+
+            var images  = '<a class="fancybox" rel="group" href="http://farm'+photoInfo.farm+'.staticflickr.com/'+photoInfo.server+'/'+photoInfo.id+'_'+photoInfo.secret+'.jpg"><img src="http://farm'+photoInfo.farm+'.staticflickr.com/'+photoInfo.server+'/'+photoInfo.id+'_'+photoInfo.secret+'_m.jpg"></a>';
+            // http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+            $('.col-12').append(images);    
+        });
+
+        $(".fancybox").fancybox();
+        
+ //        $('.col-12').packery({
+ //  			itemSelector: '.fancybox',
+ //  			gutter: 50
+	// });
+   
+
+
+   });// End of the click 
+
+
+    
 
 }); // END DOCUMENT.READY
